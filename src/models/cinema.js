77916e405b5528c8c7f5a7494784d03f3d03a78f
Meera Jose Mampilly <meerajm@gmail.com>
2020-11-17 23:16:07 +0100

    add unique to cinemaName

--- src/models/cinema.js
+++ src/models/cinema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { ShowSchema } = require("./showDetails");
 
 const CinemaSchema = new mongoose.Schema({
-  cinemaName: { type: String, required: true },
+  cinemaName: { type: String, required: true, unique: true },
   showDetails: [ShowSchema],
 });
 
