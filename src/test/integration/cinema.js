a782a22bd6fed180bd7c085da834a1e068b15f5f
Meera Jose Mampilly <meerajm@gmail.com>
2020-11-18 13:54:48 +0100

    add test to get all cinema names

--- src/test/integration/cinema.js
+++ src/test/integration/cinema.js
@@ -56,6 +56,35 @@ describe("Cinema API test", async () => {
     });
   });
 
+  describe("GET /api/v1/cinemas/all", () => {
+    it("should return all cinemas", async () => {
+      const expectedCinema = new Cinema({
+        cinemaName: "INOX",
+        showDetails: [
+          {
+            id: 1,
+            showDay: "söndag",
+            startTime: "9:30",
+            movieTitle: "Welcome",
+            screen: "1",
+            bookedSeats: ["A1", "A2", "B1"],
+          },
+        ],
+      });
+      await expectedCinema.save();
+
+      const response = await supertest(app)
+        .get("/api/v1/cinemas/all")
+        .expect(200);
+
+      const cinemaNames = await Cinema.find({}, { _id: 1, cinemaName: 1 });
+      console.log(cinemaNames[0]._id)
+      expect(response.body[0].cinemaName).to.equal(cinemaNames[0].cinemaName);
+      expect(response.body[0]._id).to.equal(cinemaNames[0]._id.toString());
+      expect(response.body[0]).to.not.include(expectedCinema.showDetails);
+    });
+  });
+
   describe("PATCH /api/v1/cinemas/:cinemaName/:showId", () => {
     it("should update a cinema's booked seat", async () => {
       const expectedCinema = new Cinema({
@@ -90,6 +119,36 @@ describe("Cinema API test", async () => {
       expect(
         JSON.stringify(findUpdatedCinema.showDetails[0].bookedSeats)
       ).to.equal(JSON.stringify(["E2", "A1", "A2", "B1"]));
+    });
+  });
+
+  describe("POST /api/v1/cinemas", () => {
+    it("should save cinema details to the databse", async () => {
+      const response = await supertest(app)
+        .post("/api/v1/cinemas")
+        .send({
+          cinemaName: "INOX",
+          showDetails: [
+            {
+              id: 1,
+              showDay: "söndag",
+              startTime: "9:30",
+              movieTitle: "Welcome",
+              screen: "1",
+              bookedSeats: ["A1", "A2", "B1"],
+            },
+          ],
+        })
+        .expect(200);
+
+      const findCinema = await Cinema.findOne({ _id: response.body._id });
+      const findCinemaId = findCinema._id.toString();
+
+      expect(response.body._id).to.equal(findCinemaId);
+      expect(response.body.cinemaName).to.equal(findCinema.cinemaName);
+      expect(JSON.stringify(response.body.showDetails[0])).to.equal(
+        JSON.stringify(findCinema.showDetails[0])
+      );
     });
   });
 });
